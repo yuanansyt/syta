fb9db0537736a51e232130d2ee3b31156c9a58d5
yuanansyt <114891782+yuanansyt@users.noreply.github.com>
2022-10-06 15:42:50 +0800

    Add files via upload

---
+++ 乘法计算器.cpp
@@ -0,0 +1,18 @@
+#include <conio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int a=0;
+	int b=0;
+	
+	scanf("%d%d",&a,&b);
+	
+	int resul=a*b;
+	
+	
+	printf("%d*%d=%d",a,b,resul);
+	
+	return 0;
+}
